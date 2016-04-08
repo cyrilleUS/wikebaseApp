240badd706c7c403629e4ab2c2fde11f84ec8fd7
cyrilleUS <cyrille.vanhulle.us@gmail.com>
2016-04-08 15:03:19 +0200

    Premier commit ! Un jour ça marchera promis !

---
+++ app/pages/home/home.ts
@@ -0,0 +1,16 @@
+import {Page} from 'ionic-angular';
+
+
+@Page({
+  templateUrl: 'build/pages/home/home.html',
+})
+
+export class Home {
+  categories:any[];
+  searchCategories: any;
+  currentCategory: any = "All";
+  searchString: any;
+  constructor() {
+    this.categories = ["All", "Video", "Music", "Marketing", "Copywriting"];
+  }
+}
